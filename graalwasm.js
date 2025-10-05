f67b70ab4d71c24229ba4265affa0b16a4a27def
lnwza007⚡️ <120770468+rushmi0@users.noreply.github.com>
2025-10-05 17:20:58 +0700

    Create graalwasm.js

---
+++ graalwasm.js
@@ -0,0 +1,33 @@
+var HostAccess = Java.type("org.graalvm.polyglot.HostAccess");
+var Context = Java.type("org.graalvm.polyglot.Context");
+var Source = Java.type("org.graalvm.polyglot.Source");
+var URL = Java.type("java.net.URL");
+
+/*
+  ;; wat2wasm add-two.wat -o add-two.wasm
+  (module
+    (func (export "addTwo") (param i32 i32) (result i32)
+      local.get 0
+      local.get 1
+      i32.add
+    )
+  )
+*/
+var wasmUrl = new URL("file:///C:/app/graalwasm/add-two.wasm");
+
+var context = Context.newBuilder("wasm")
+    .allowHostAccess(HostAccess.ALL)
+    .allowHostClassLookup(function(className) { return true; })
+    .build();
+
+var source = Source.newBuilder("wasm", wasmUrl).build();
+
+var mainModule = context.eval(source);
+var addTwo = mainModule.getMember("addTwo");
+
+if (addTwo == null) {
+    TALON.addMsg("Function 'addTwo' not found in wasm module.");
+} else {
+    var result = addTwo.execute(40, 2);
+    TALON.addMsg("addTwo(40, 2) = " + result.asInt());
+}
